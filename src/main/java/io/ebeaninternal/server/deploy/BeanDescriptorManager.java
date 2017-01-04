530cd3a4bd3d98dd81e5cc678908934851dcab99
Roland Praml <praml@foconis.de>
2017-01-04 10:00:52 +0100

    ENH: Add CustomAnnotationParser

--- src/main/java/io/ebeaninternal/server/deploy/BeanDescriptorManager.java
+++ src/main/java/io/ebeaninternal/server/deploy/BeanDescriptorManager.java
@@ -38,6 +38,7 @@ import io.ebeaninternal.server.deploy.meta.DeployBeanPropertyAssocMany;
 import io.ebeaninternal.server.deploy.meta.DeployBeanPropertyAssocOne;
 import io.ebeaninternal.server.deploy.meta.DeployBeanTable;
 import io.ebeaninternal.server.deploy.meta.DeployTableJoin;
+import io.ebeaninternal.server.deploy.parse.AnnotationCustomDeploy;
 import io.ebeaninternal.server.deploy.parse.DeployBeanInfo;
 import io.ebeaninternal.server.deploy.parse.DeployCreateProperties;
 import io.ebeaninternal.server.deploy.parse.DeployInherit;
@@ -319,6 +320,7 @@ public class BeanDescriptorManager implements BeanDescriptorMap {
       readEntityBeanTable();
       readEntityDeploymentAssociations();
       readInheritedIdGenerators();
+      runCustomDeploy(); 
       // creates the BeanDescriptors
       readEntityRelationships();
 
@@ -762,6 +764,12 @@ public class BeanDescriptorManager implements BeanDescriptorMap {
     }
   }
 
+  private void runCustomDeploy() {
+    for (Map.Entry<Class<?>, DeployBeanInfo<?>> entry : deployInfoMap.entrySet()) {
+      new AnnotationCustomDeploy(entry.getValue(), 
+          serverConfig.getClassLoadConfig().isJavaxValidationAnnotationsPresent()).parse();
+    }
+  }
   /**
    * Sets the inheritance info. ~EMG fix for join problem
    *
---
+++ src/main/java/io/ebeaninternal/server/deploy/parse/AnnotationCustomDeploy.java
@@ -0,0 +1,50 @@
+package io.ebeaninternal.server.deploy.parse;
+
+import io.ebean.annotation.CustomAnnotationParser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.util.Set;
+
+/**
+ * AnnotationCustomDeploy is executed after all DeployBeanInfo is created. 
+ */
+public class AnnotationCustomDeploy extends AnnotationParser {
+
+  private static final Logger logger = LoggerFactory.getLogger(AnnotationCustomDeploy.class);
+
+
+
+  /**
+   * Create for normal early parse of class level annotations.
+   */
+  public AnnotationCustomDeploy(DeployBeanInfo<?> info, boolean validationAnnotations) {
+    super(info, validationAnnotations);
+
+  }
+
+
+  /**
+   * Read the class level deployment annotations.
+   */
+  public void parse() {
+    Set<CustomAnnotationParser> customParserAnnotations = AnnotationBase.findAnnotations(descriptor.getBeanType(), CustomAnnotationParser.class);
+    for (CustomAnnotationParser customParserAnnotation : customParserAnnotations) {
+      for (Class<? extends AnnotationParser> annotationParserClass : customParserAnnotation.value()) {
+        createInstance(annotationParserClass).parse();
+      }
+    }
+  }
+
+  private AnnotationParser createInstance(Class<? extends AnnotationParser> customDeployParserClass) {
+    try {
+      Constructor<? extends AnnotationParser> ctor = customDeployParserClass.getConstructor(DeployBeanInfo.class, Boolean.TYPE);
+      return ctor.newInstance(info, validationAnnotations);
+    } catch (Exception e) {
+      throw new RuntimeException("Cannot create instance " + customDeployParserClass, e);
+    }
+  }
+
+}
