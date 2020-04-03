843df8903cdf3f37bd497c257ad22f51ec481ab1
Jonas Pöhler (JPo) <jonas.poehler@foconis.de>
2020-04-03 14:04:30 +0200

    ADD: failing testcase for @OrderColumn working when referencing the parent of a inheritance structure

---
+++ src/test/java/org/tests/inheritance/TestInheritanceOrderColumn.java
@@ -0,0 +1,63 @@
+package org.tests.inheritance;
+
+import io.ebean.BaseTestCase;
+import io.ebean.Ebean;
+import io.ebeantest.LoggedSql;
+import org.ebeantest.LoggedSqlCollector;
+import org.junit.Test;
+import org.tests.inheritance.order.OrderMaster;
+import org.tests.inheritance.order.OrderedA;
+import org.tests.inheritance.order.OrderedB;
+import org.tests.inheritance.order.OrderedParent;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestInheritanceOrderColumn extends BaseTestCase {
+
+  @Test
+  public void test() {
+    final OrderMaster master = new OrderMaster();
+    final OrderedA orderedA = new OrderedA();
+    orderedA.setCommonName("commonOrderedA");
+    orderedA.setOrderedAName("orderedA");
+
+    final OrderedB orderedB = new OrderedB();
+    orderedB.setCommonName("commonOrderedB");
+    orderedB.setOrderedBName("orderedB");
+
+    master.getReferenced().add(orderedA);
+    master.getReferenced().add(orderedB);
+
+    LoggedSqlCollector.start();
+    Ebean.save(master);
+    List<String> sql = LoggedSqlCollector.stop();
+    assertThat(sql).hasSize(5);
+    assertThat(sql.get(1)).contains("insert into ordered_parent (order_master_id, dtype, common_name, ordered_aname, sort_order)");
+    assertThat(sql.get(3)).contains("insert into ordered_parent (order_master_id, dtype, common_name, ordered_bname, sort_order)");
+
+    OrderMaster result = Ebean.find(OrderMaster.class).findOne();
+    assertThat(result.getReferenced())
+      .extracting(OrderedParent::getCommonName)
+      .containsExactly("commonOrderedA", "commonOrderedB");
+
+    // Swap the two
+    result.getReferenced().add(0, result.getReferenced().remove(1));
+    assertThat(result.getReferenced())
+      .extracting(OrderedParent::getCommonName)
+      .containsExactly("commonOrderedB", "commonOrderedA");
+
+    LoggedSql.start();
+    Ebean.save(result);
+    sql = LoggedSqlCollector.stop();
+    assertThat(sql).hasSize(3);
+    assertThat(sql.get(0)).contains("update ordered_parent set sort_order=? where id=?");
+
+    result = Ebean.find(OrderMaster.class).findOne();
+    assertThat(result.getReferenced())
+      .extracting(OrderedParent::getCommonName)
+      .containsExactly("commonOrderedB", "commonOrderedA");
+  }
+
+}
