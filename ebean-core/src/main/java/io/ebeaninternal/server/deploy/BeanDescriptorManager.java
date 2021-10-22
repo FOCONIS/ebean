8fe38f9160aeeabb78b07ff489d25ab8bcf9d9e2
Jonas Pöhler <jonas.poehler@foconis.de>
2021-10-22 15:04:46 +0200

    CustomDeployParser base code
    CustomDeployParser fixes

--- ebean-core/src/main/java/io/ebeaninternal/server/deploy/BeanDescriptorManager.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/deploy/BeanDescriptorManager.java
@@ -75,6 +75,7 @@ public final class BeanDescriptorManager implements BeanDescriptorMap, SpiBeanTy
   private final BeanFinderManager beanFinderManager;
   private final PersistListenerManager persistListenerManager;
   private final BeanQueryAdapterManager beanQueryAdapterManager;
+  private final CustomDeployParserManager customDeployParserManager;
   private final NamingConvention namingConvention;
   private final DeployCreateProperties createProperties;
   private final BeanManagerFactory beanManagerFactory;
@@ -155,6 +156,7 @@ public final class BeanDescriptorManager implements BeanDescriptorMap, SpiBeanTy
     this.persistListenerManager = new PersistListenerManager(bootupClasses);
     this.beanQueryAdapterManager = new BeanQueryAdapterManager(bootupClasses);
     this.beanFinderManager = new BeanFinderManager(bootupClasses);
+    this.customDeployParserManager = new CustomDeployParserManager(bootupClasses);
     this.transientProperties = new TransientProperties();
     this.changeLogPrepare = config.changeLogPrepare(bootupClasses.getChangeLogPrepare());
     this.changeLogListener = config.changeLogListener(bootupClasses.getChangeLogListener());
@@ -293,6 +295,7 @@ public final class BeanDescriptorManager implements BeanDescriptorMap, SpiBeanTy
       readEntityBeanTable();
       readEntityDeploymentAssociations();
       readInheritedIdGenerators();
+      deployInfoMap.values().forEach(customDeployParserManager::parse);
       // creates the BeanDescriptors
       readEntityRelationships();
       List<BeanDescriptor<?>> list = new ArrayList<>(descMap.values());
