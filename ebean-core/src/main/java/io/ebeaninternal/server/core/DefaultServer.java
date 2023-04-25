7b2669519e494e56e05f067f7bb14c321c09c21b
Roland Praml <roland.praml@foconis.de>
2023-04-25 14:05:36 +0200

    NEW: DDL generation can be invoked separately
    Fix for css

--- ebean-core/src/main/java/io/ebeaninternal/server/core/DefaultServer.java
+++ ebean-core/src/main/java/io/ebeaninternal/server/core/DefaultServer.java
@@ -2281,4 +2281,9 @@ public final class DefaultServer implements SpiServer, SpiEbeanServer {
   List<MetaQueryPlan> queryPlanCollectNow(QueryPlanRequest request) {
     return queryPlanManager.collect(request);
   }
+
+  @Override
+  public void runDdl() {
+    ddlGenerator.runDdl();
+  }
 }
