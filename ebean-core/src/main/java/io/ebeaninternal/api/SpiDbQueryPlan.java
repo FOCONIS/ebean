b1a2778bfc40fbf7f99f17d40f2642d2568729da
Rob Bygrave <130515035+rob-bygrave@users.noreply.github.com>
2023-05-16 15:38:05 +1200

    Support for queryCaptureMicros and whenCaptured

--- ebean-core/src/main/java/io/ebeaninternal/api/SpiDbQueryPlan.java
+++ ebean-core/src/main/java/io/ebeaninternal/api/SpiDbQueryPlan.java
@@ -2,6 +2,8 @@ package io.ebeaninternal.api;
 
 import io.ebean.meta.MetaQueryPlan;
 
+import java.time.Instant;
+
 /**
  * Internal database query plan being capture.
  */
@@ -10,6 +12,6 @@ public interface SpiDbQueryPlan extends MetaQueryPlan {
   /**
    * Extend with queryTimeMicros and captureCount.
    */
-  SpiDbQueryPlan with(long queryTimeMicros, long captureCount);
+  SpiDbQueryPlan with(long queryTimeMicros, long captureCount, long captureMicros, Instant whenCaptured);
 
 }
