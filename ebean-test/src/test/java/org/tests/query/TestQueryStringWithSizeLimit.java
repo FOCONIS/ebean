5c99fd8d0a2ab414da3c71d3b6c06da824702a3d
Noemi Praml <noemi.praml@foconis.de>
2024-10-17 11:16:58 +0200

    temp commit

--- ebean-test/src/test/java/org/tests/query/TestQueryStringWithSizeLimit.java
+++ ebean-test/src/test/java/org/tests/query/TestQueryStringWithSizeLimit.java
@@ -5,6 +5,7 @@ import io.ebean.ExpressionList;
 import io.ebean.Query;
 import io.ebean.test.LoggedSql;
 import io.ebean.xtest.BaseTestCase;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.tests.model.basic.Customer;
 import org.tests.model.basic.ResetBasicData;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Disabled
 public class TestQueryStringWithSizeLimit extends BaseTestCase {
 
   private String SEARCH_VALUE = "a".repeat(41);
@@ -24,15 +26,16 @@ public class TestQueryStringWithSizeLimit extends BaseTestCase {
     ResetBasicData.reset();
 
     Query<Customer> query = DB.find(Customer.class).select("id,name,status").where().eq("name", SEARCH_VALUE).query();
-    List<Customer> list = query.findList();
+    query.findList();
     assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where 1=0");
 
     query = DB.find(Customer.class).select("id,name,status").where().eq("name", "Rob").query();
-    list = query.findList();
+    query.findList();
     assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name = ?");
 
-    //assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
+    query = DB.find(Customer.class).select("id,name,status").where().not().eq("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (1=0)");
 
   }
 
@@ -41,29 +44,188 @@ public class TestQueryStringWithSizeLimit extends BaseTestCase {
 
     ResetBasicData.reset();
 
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().ne("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().ne("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where 1=0");
 
-    assertThat(list).isEmpty();
-    //assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
+    query = DB.find(Customer.class).select("id,name, status").where().ne("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name <> ?");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().ne("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (1=0)");
 
   }
 
+  @Test
+  public void testStartsWith() {
+
+    ResetBasicData.reset();
+
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().startsWith("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name like ? escape'|'"); //FIXME: 1 = 0
+
+    query = DB.find(Customer.class).select("id,name, status").where().startsWith("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name like ? escape'|'");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().startsWith("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (t0.name like ? escape'|')"); // FIXME: not (1=0)
+
+  }
+
+  @Test
+  public void testIStartsWith() {
+
+    ResetBasicData.reset();
+
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().istartsWith("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where lower(t0.name) like ? escape'|'"); //FIXME: 1 = 0
+
+    query = DB.find(Customer.class).select("id,name, status").where().istartsWith("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where lower(t0.name) like ? escape'|'");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().istartsWith("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (lower(t0.name) like ? escape'|')"); // FIXME: not (1=0)
+
+  }
+
+  @Test
+  public void testEndsWith() {
+
+    ResetBasicData.reset();
+
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().endsWith("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name like ? escape'|'"); //FIXME: 1 = 0
+
+    query = DB.find(Customer.class).select("id,name, status").where().endsWith("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name like ? escape'|'");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().endsWith("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (t0.name like ? escape'|')"); // FIXME: not (1=0)
+
+  }
+
+  @Test
+  public void testIEndsWith() {
+
+    ResetBasicData.reset();
+
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().iendsWith("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where lower(t0.name) like ? escape'|'"); //FIXME: 1 = 0
+
+    query = DB.find(Customer.class).select("id,name, status").where().iendsWith("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where lower(t0.name) like ? escape'|'");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().iendsWith("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (lower(t0.name) like ? escape'|')"); // FIXME: not (1=0)
+
+  }
+
+  @Test
+  public void testContains() {
+
+    ResetBasicData.reset();
+
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().contains("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name like ? escape'|'"); //FIXME: 1 = 0
+
+    query = DB.find(Customer.class).select("id,name, status").where().contains("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name like ? escape'|'");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().contains("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (t0.name like ? escape'|')"); // FIXME: not (1=0)
+
+  }
+
+  @Test
+  public void testIContains() {
+
+    ResetBasicData.reset();
+
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().icontains("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where lower(t0.name) like ? escape'|'"); //FIXME: 1 = 0
+
+    query = DB.find(Customer.class).select("id,name, status").where().icontains("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where lower(t0.name) like ? escape'|'");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().icontains("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (lower(t0.name) like ? escape'|')"); // FIXME: not (1=0)
+
+  }
+
+  @Test
+  public void testLike() {
+
+    ResetBasicData.reset();
+
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().like("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name like ? escape''"); //FIXME: 1 = 0
+
+    query = DB.find(Customer.class).select("id,name, status").where().like("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name like ? escape''");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().like("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (t0.name like ? escape'')"); // FIXME: not (1=0)
+
+  }
+
+  @Test
+  public void testILike() {
+
+    ResetBasicData.reset();
+
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().ilike("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where lower(t0.name) like ? escape''"); //FIXME: 1 = 0
+
+    query = DB.find(Customer.class).select("id,name, status").where().ilike("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where lower(t0.name) like ? escape''");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().ilike("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (lower(t0.name) like ? escape'')"); // FIXME: not (1=0)
+
+  }
 
   @Test
   public void testIn() {
 
     ResetBasicData.reset();
 
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().in("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
+    Query<Customer> query = DB.find(Customer.class).select("id,name, status").where().in("name", SEARCH_VALUE).query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where 1=0");
 
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(5);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
+    query = DB.find(Customer.class).select("id,name, status").where().in("name", "Rob").query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where t0.name in (?)");
+
+    query = DB.find(Customer.class).select("id,name, status").where().not().in("name", SEARCH_VALUE).endNot().query();
+    query.findList();
+    assertThat(query.getGeneratedSql()).isEqualTo("select t0.id, t0.name, t0.status from o_customer t0 where not (1=0)");
 
   }
 
@@ -74,126 +236,6 @@ public class TestQueryStringWithSizeLimit extends BaseTestCase {
 
     LoggedSql.start();
     List<Customer> list = DB.find(Customer.class).select("id,name, status").where().notIn("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
-
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
-
-  }
-
-  @Test
-  public void testStartsWith() {
-
-    ResetBasicData.reset();
-
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().startsWith("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
-
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
-
-  }
-
-  @Test
-  public void testIStartsWith() {
-
-    ResetBasicData.reset();
-
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().istartsWith("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
-
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
-
-  }
-
-  @Test
-  public void testEndsWith() {
-
-    ResetBasicData.reset();
-
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().endsWith("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
-
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
-
-  }
-
-  @Test
-  public void testIEndsWith() {
-
-    ResetBasicData.reset();
-
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().iendsWith("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
-
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
-
-  }
-
-  @Test
-  public void testContains() {
-
-    ResetBasicData.reset();
-
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().contains("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
-
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
-
-  }
-
-  @Test
-  public void testIContains() {
-
-    ResetBasicData.reset();
-
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().icontains("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
-
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
-
-  }
-
-  @Test
-  public void testLike() {
-
-    ResetBasicData.reset();
-
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().like("name", DB2_SEARCH_VALUE).findList();
-    List<String> stop = LoggedSql.stop();
-
-    assertThat(list).isEmpty();
-    assertThat(stop).hasSize(1);
-    //assertThat(stop.get(0)).contains("--bind(" + SEARCH_VALUE);
-
-  }
-
-  @Test
-  public void testILike() {
-
-    ResetBasicData.reset();
-
-    LoggedSql.start();
-    List<Customer> list = DB.find(Customer.class).select("id,name, status").where().ilike("name", DB2_SEARCH_VALUE).findList();
     List<String> stop = LoggedSql.stop();
 
     assertThat(list).isEmpty();
