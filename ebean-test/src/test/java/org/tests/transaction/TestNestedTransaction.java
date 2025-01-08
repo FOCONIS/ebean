83c183a6d051bd3d8c3a85c3ccb59ec01a414578
Noemi Praml <noemi.praml@foconis.de>
2025-01-08 14:12:49 +0100

    Add: failing Unittest

--- ebean-test/src/test/java/org/tests/transaction/TestNestedTransaction.java
+++ ebean-test/src/test/java/org/tests/transaction/TestNestedTransaction.java
@@ -284,4 +284,18 @@ public class TestNestedTransaction extends BaseTestCase {
     }
   }
 
+  @Test
+  public void test_txn_with_BatchMode() {
+
+    try (Transaction txn1 = DB.beginTransaction(TxScope.requiresNew())) {
+
+      try (Transaction txn2 = DB.beginTransaction()) {
+        txn2.setBatchMode(true);
+        txn2.commit();
+      }
+      // resume txn1
+      assertThat(txn1.isBatchMode()).isFalse();
+    }
+  }
+
 }
